66f03e1bdc90f532a5debc62ecca2a7f355022af
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-20 15:40:30 +0000

    test

---
+++ main.py
@@ -0,0 +1,38 @@
+# name = input('What is your name?')
+# print('hello ' + name)
+# print(type(2 + 4))
+# print(4 + 4)
+# print(type(6 / 2))
+# print(14 // 3)
+# print(14% 3)
+# print(round(1.67))
+# print(abs(-20))
+iq = 190
+# variable
+print(iq)
+# constant
+PI = 3.14 
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+some_value = 5
+some_value += 2
+print(some_value)
+first_name = 'Krystian'
+last_name = 'Żywczak'
+full_name = first_name + " " + last_name
+print(full_name)
+weather = "\t it\'s \"kind of\" sunny \n hope You have a good day!"
+print(weather)
+
+name = 'Johnny'
+age = 55
+print(f'hey {name}, You are {age} years old')
+print('hey {}, You are {} years old'.format(name, age))
+print('hey {1}, You are {0} years old'.format(name, age))
+selfish = 'me me me'
+print(selfish[0:5])
+print(selfish[0:8:3])
+print(selfish[::-1])
+# 1.26
