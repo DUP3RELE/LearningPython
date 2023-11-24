693c12907577ba0210f1a95e5f9741354e039650
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-24 16:24:25 +0000

    elif and 'and'

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 is_old = False
-if is_old:
-  print('you are old enough to drive')
+is_licenced = True
+# if is_old:
+#   print('you are old enough to drive')
+# elif is_licenced:
+#   print('You can drive now')
+# else:
+#   print('you are not old enough to drive!')
+
+if is_old and is_licenced:
+  print('You are ready to drive')  
 else:
-  print('you are not old enough to drive!')
+    print('you are not old enough to drive!')
