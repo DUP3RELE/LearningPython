b73e9c82e9c2fea762e6a1a1074d5de6ecd964c7
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-25 08:57:40 +0000

    is magician or not?

---
+++ magicTask.py
@@ -0,0 +1,11 @@
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+  print('You are master magician!')
+
+if is_magician and not(is_expert):
+  print('At least You\'re getting there')
+
+if not(is_magician):
+  print('You need to learn magic')
--- main.py
+++ main.py
@@ -12,13 +12,24 @@
 # else:
 #     print('you are not old enough to drive!')
 
-is_friend = False
-is_user = True
-can_message = "message allowed" if  is_friend else 'message not allowed'
-print(can_message)
-# 30:00
-# short circuting
-if is_friend or is_user:
-  print('best friends forever!')
-if is_friend and is_user:
-  print('best friends forever!')
+# is_friend = False
+# is_user = True
+# can_message = "message allowed" if  is_friend else 'message not allowed'
+# print(can_message)
+# # 30:00
+# # short circuting
+# if is_friend or is_user:
+#   print('best friends forever!')
+# if is_friend and is_user:
+#   print('best friends forever!')
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert:
+  print('You are master magician!')
+
+if is_magician and not(is_expert):
+  print('At least You\'re getting there')
+
+if not(is_magician):
+  print('You need to learn magic')
