c6779e23748423222b7de755c48a3b6669445774
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-04 15:13:55 +0000

    1

--- main.py
+++ main.py
@@ -168,13 +168,3 @@ def outer():
   inner()
   print("outer: ", x)
 outer()
-
-def outer():
-  x = 'local'
-  def inner():
-    nonlocal x
-    x = 'nonlocal'
-    print("inner: ", x)
-  inner()
-  print("outer: ", x)
-outer()
