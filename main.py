345e9e680559c4824e67724ef715b839d6a68b44
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-11 16:10:11 +0000

    list exercise

--- main.py
+++ main.py
@@ -244,22 +244,32 @@
 # # introspection
 # print(dir(wizard1))
 
-class Toy():
-  def __init__(self, color, age):
-    self.color = color
-    self.age = age
-    self.my_dict = {
-      'name':'Yoyo',
-      'has pets':False
-    }
+# class Toy():
+#   def __init__(self, color, age):
+#     self.color = color
+#     self.age = age
+#     self.my_dict = {
+#       'name':'Yoyo',
+#       'has pets':False
+#     }
 
-  def __str__(self):
-    return f'{self.color}'
+#   def __str__(self):
+#     return f'{self.color}'
 
-  def __getitem__(self, i):
-    return self.my_dict[i]
+#   def __getitem__(self, i):
+#     return self.my_dict[i]
   
-action_figure = Toy('red', 13)
-print(action_figure.__str__())
-print(str(action_figure))
-print(action_figure['name'])
+# action_figure = Toy('red', 13)
+# print(action_figure.__str__())
+# print(str(action_figure))
+# print(action_figure['name'])
+
+class SuperList(list):
+  def __len__(self):
+    return 1000
+
+super_list1 = SuperList()
+print(len(super_list1))
+super_list1.append(5)
+print(super_list1[0])
+print(issubclass(SuperList, list))
