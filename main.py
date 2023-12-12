35acb081d9627eb5863b4aed30f4a1081322cde8
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-12 13:43:45 +0000

    Sort list exercise

--- main.py
+++ main.py
@@ -302,24 +302,37 @@
 
 # print(hybrid1.sign_in())
 # print(hybrid1.attack())
-from functools import reduce
-new_list = [1,2,3]
-your_list = [10,20,30]
-their_list = [5,4,3]
+# from functools import reduce
+# new_list = [1,2,3]
+# your_list = [10,20,30]
+# their_list = [5,4,3]
 
-def multiply_by2(item):
-  return item*2
+# def multiply_by2(item):
+#   return item*2
 
-def check_odd(item):
-  return item % 2 != 0
+# def check_odd(item):
+#   return item % 2 != 0
 
-def accumulator(acc, item):
-  return acc + item
+# def accumulator(acc, item):
+#   return acc + item
 
-print(list(map(multiply_by2, new_list)))
-print(list(filter(check_odd, new_list)))
-print(new_list)
-print(list(zip(new_list, your_list, their_list)))
-print(reduce(accumulator, new_list, 0))
-# lambda - one time anonymous function
-print(list(map(lambda item: item*2, new_list)))
+# print(list(map(multiply_by2, new_list)))
+# print(list(filter(check_odd, new_list)))
+# print(new_list)
+# print(list(zip(new_list, your_list, their_list)))
+# print(reduce(accumulator, new_list, 0))
+# # lambda - one time anonymous function
+# print(list(map(lambda item: item*2, new_list)))
+
+# square
+
+my_list = [5,4,3]
+
+print(list(map(lambda item: item**2, my_list)))
+
+# list sorting
+
+a = [(0,2), (4,3), (9,9), (10,-1)]
+
+a.sort(key= lambda x: x[1])
+print(a)
