934c49f7fb6e03d511a8383801266f8a70ff59e8
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-12 14:01:32 +0000

    FP - dictionary

--- main.py
+++ main.py
@@ -352,5 +352,6 @@ simple_dict = {
   'd':4
 }
 my_dict = {key:value**2 for key,value in simple_dict.items() if value % 2 == 0}
-
+your_dict = {num:num*2 for num in [1,2,3]}
 print(my_dict)
+print(your_dict)
