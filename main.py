614083cd5b953f1e4b62ab5c0dda7dd255e197b2
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-17 11:48:16 +0000

    performance decorator!

--- main.py
+++ main.py
@@ -394,3 +394,20 @@ def hello(greeting, emoji):
   print(greeting, emoji)
 
 hello('hi', ':P')
+
+from time import time
+def performance(fn):
+  def wrapper(*args, **kwargs):
+    t1 = time()
+    result = fn(*args, **kwargs)
+    t2 = time()
+    print(f'took {t2-t1} ms')
+    return result
+  return wrapper
+
+@performance
+def long_time():
+  for i in range(1000000):
+    i*5
+
+long_time()
