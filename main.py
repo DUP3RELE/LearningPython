7b2b2336895e01ebb1c24c445339ebc2ae859d51
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-18 15:56:31 +0000

    error handling = except rule

--- main.py
+++ main.py
@@ -382,32 +382,38 @@
 # hello
 
 
-def my_decorator(func):
-  def wrap_func(*args, **kwargs):
-    print('****')
-    func(*args, **kwargs)
-    print('****')
-  return wrap_func
+# def my_decorator(func):
+#   def wrap_func(*args, **kwargs):
+#     print('****')
+#     func(*args, **kwargs)
+#     print('****')
+#   return wrap_func
 
-@my_decorator
-def hello(greeting, emoji):
-  print(greeting, emoji)
+# @my_decorator
+# def hello(greeting, emoji):
+#   print(greeting, emoji)
 
-hello('hi', ':P')
+# hello('hi', ':P')
 
-from time import time
-def performance(fn):
-  def wrapper(*args, **kwargs):
-    t1 = time()
-    result = fn(*args, **kwargs)
-    t2 = time()
-    print(f'took {t2-t1} ms')
-    return result
-  return wrapper
+# from time import time
+# def performance(fn):
+#   def wrapper(*args, **kwargs):
+#     t1 = time()
+#     result = fn(*args, **kwargs)
+#     t2 = time()
+#     print(f'took {t2-t1} ms')
+#     return result
+#   return wrapper
 
-@performance
-def long_time():
-  for i in range(1000000):
-    i*5
+# @performance
+# def long_time():
+#   for i in range(1000000):
+#     i*5
 
-long_time()
+# long_time()
+
+try:
+  age = int(input('what is Your age?: '))
+  print(age)
+except:
+  print('Please enter a number')
