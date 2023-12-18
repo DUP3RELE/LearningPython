4ce5312ed97ecded144f259e6418b7c1a90e6c5b
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-18 22:03:47 +0000

    range class

--- main.py
+++ main.py
@@ -444,12 +444,40 @@
 
 # print(sum('1', 2))
 
-def generator_function(num):
-  for i in range(num):
-    yield i*2
+# def generator_function(num):
+#   for i in range(num):
+#     yield i*2
 
-g = generator_function(100)
-next(g)
-next(g)
-print(next(g))
-  
+# g = generator_function(100)
+# next(g)
+# next(g)
+# print(next(g))
+
+def special_for(iterable):
+  iterator = iter(iterable)
+  while True:
+    try:
+      print(iterator)
+      next(iterator)
+    except StopIteration:
+      break
+
+special_for([1,2,3])
+
+class MyGen():
+  current = 0
+  def __init__(self, first, last):
+    self.first = first
+    self.last = last
+  def __iter__(self):
+    return self
+  def __next__(self):
+    if MyGen.current < self.last:
+      num = MyGen.current
+      MyGen.current += 1
+      return num
+    raise StopIteration
+
+gen = MyGen(0,100)
+for i in gen:
+  print(i)
