bd5a3d51a3395370ddf088cc4d37df2440431fdd
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-22 17:20:55 +0000

    Modules

--- main.py
+++ main.py
@@ -476,27 +476,32 @@
 #   print(i)
 
 
-def fib(number):
-  a = 0
-  b = 1
-  for i in range(number):
-    yield a
-    temp = a
-    a = b
-    b = temp + b
+# def fib(number):
+#   a = 0
+#   b = 1
+#   for i in range(number):
+#     yield a
+#     temp = a
+#     a = b
+#     b = temp + b
 
-def fib2(number):
-  a = 0
-  b = 1
-  result = []
-  for i in range(number):
-    result.append(a)
-    temp = a
-    a = b
-    b = temp + b
-  return result
+# def fib2(number):
+#   a = 0
+#   b = 1
+#   result = []
+#   for i in range(number):
+#     result.append(a)
+#     temp = a
+#     a = b
+#     b = temp + b
+#   return result
 
-for x in fib(20):
-  print(x)
+# for x in fib(20):
+#   print(x)
 
-print(fib2(20))
+# print(fib2(20))
+
+import utility
+
+print(utility.multiply(2,3))
+print(utility.divide(6,2))
