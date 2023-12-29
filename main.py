823aa365e49f68ed8c5b26a656c8e2ee5cb1e88a
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-12-29 20:49:43 +0000

    installing modules - pyjokes

--- main.py
+++ main.py
@@ -517,10 +517,16 @@
 # print(random.randint(1,10))
 # print(my_list)
 
-import random
-random_num = random.randint(1,10)
-user_num = int(input('guess the number from 1 to 10: '))
-if random_num == user_num:
-  print("you got it champ!")
-else:
-  print(f'you missed! The number was {random_num}')
+# import random
+# random_num = random.randint(1,10)
+# user_num = int(input('guess the number from 1 to 10: '))
+# if random_num == user_num:
+#   print("you got it champ!")
+# else:
+#   print(f'you missed! The number was {random_num}')
+
+import pyjokes
+
+joke = pyjokes.get_joke('en', 'neutral')
+
+print(joke)
