8705d93e5a84e5f53f61d3b6314352e288419032
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-08 11:11:56 +0000

    testing guess game

--- main.py
+++ main.py
@@ -657,11 +657,12 @@ def run_guess(guess, answer):
   else:
     print('hey bozo, I said 1~100')
 
-answer = random.randint(1,10)
-while True:
-  try:
-    guess = int(input('Guess a number 1~10: '))
-    run_guess(guess, answer)
-  except ValueError:
-    print('please enter a number')
-    continue
+if __name__ == '__main__':
+  answer = random.randint(1,10)
+  while True:
+    try:
+      guess = int(input('Guess a number 1~10: '))
+      run_guess(guess, answer)
+    except ValueError:
+      print('please enter a number')
+      continue
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import unittest
+import script
 
 class TestGame(unittest.TestCase):
   def test_input(self):
-    pass
+    answer = 5
+    guess = 5
+    result = script.run_guess(answer, guess)
+    self.assertTrue(result)
 
 if __name__ == '__main__':
   unittest.main()
