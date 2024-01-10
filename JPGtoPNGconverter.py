9fd3950f519d8e42faa5d0e4c3b359fe39185ce8
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-10 10:45:17 +0000

    second solution

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -9,10 +9,14 @@ second_arg = sys.argv[2]
 if not os.path.exists(second_arg):
   os.makedirs(second_arg)
 
-for item in os.listdir(first_arg):
-  if item.endswith(".jpg"):
-    img = Image.open(os.path.join(first_arg, item))
-    output_filename = os.path.splitext(item)[0] + ".png"
-    output_path = os.path.join(second_arg, output_filename)
-    img.save(output_path, "PNG")
+# for item in os.listdir(first_arg):
+#   if item.endswith(".jpg"):
+#     img = Image.open(os.path.join(first_arg, item))
+#     output_filename = os.path.splitext(item)[0] + ".png"
+#     output_path = os.path.join(second_arg, output_filename)
+#     img.save(output_path, "PNG")
 
+for item in os.listdir(first_arg):
+  img = Image.open(f"{first_arg}/{item}")
+  clean_name = os.path.splitext(item)[0]
+  img.save(f'{second_arg}/{clean_name}.png', 'png')
