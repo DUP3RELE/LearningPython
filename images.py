d0f7b1f533b9cc76f719468668a7c258743cd5aa
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-10 10:08:09 +0000

    img thumbnail

--- images.py
+++ images.py
@@ -1,7 +1,12 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./image.jpg')
-# filetered_img = img.filter(ImageFilter.SMOOTH)
-filetered_img = img.convert('L')
-print(img)
-filetered_img.save('grey.png', 'png')
+# img = Image.open('./image.jpg')
+# # filetered_img = img.filter(ImageFilter.SMOOTH)
+# filetered_img = img.convert('L')
+# print(img)
+# filetered_img.save('grey.png', 'png')
+# filetered_img.show()
+
+img = Image.open('./astro.jpg')
+img.thumbnail((400, 400))
+img.save('thumbnail.jpg')
