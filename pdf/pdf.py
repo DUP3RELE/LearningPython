e9d4c94c0f572f39336024f9066fc6101ce90573
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-10 11:25:40 +0000

    usefull pdf merger

--- pdf/pdf.py
+++ pdf/pdf.py
@@ -1,11 +1,22 @@
 import PyPDF2
+import sys
 
-with open('pdf/dummy.pdf', 'rb') as file:
-  reader = PyPDF2.PdfReader(file)
-  page = reader.pages[0]
-  print(page.rotate(180))
-  writer = PyPDF2.PdfWriter()
-  writer.add_page(page)
-  with open('pdf/tilt.pdf', 'wb') as new_file:
-    writer.write(new_file)
-    
+imputs = sys.argv[1:]
+
+# with open('pdf/dummy.pdf', 'rb') as file:
+#   reader = PyPDF2.PdfReader(file)
+#   page = reader.pages[0]
+#   print(page.rotate(180))
+#   writer = PyPDF2.PdfWriter()
+#   writer.add_page(page)
+#   with open('pdf/tilt.pdf', 'wb') as new_file:
+#     writer.write(new_file)
+
+def pdf_combiner(pdf_list):
+  merger = PyPDF2.PdfMerger()
+  for pdf in pdf_list:
+    print(pdf)
+    merger.append(pdf)
+  merger.write('pdf/super.pdf')
+
+pdf_combiner(imputs)
