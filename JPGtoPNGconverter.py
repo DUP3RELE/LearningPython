58f849cb79857bc76c98356b85ddf49f4209b561
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-10 10:37:58 +0000

    jpg to png converter simplified a bit

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,19 +2,17 @@ import sys
 import os
 from PIL import Image
 
-if len(sys.argv) >= 3:
-  first_arg = sys.argv[1]
-  second_arg = sys.argv[2]
 
-  if not os.path.exists(second_arg):
-    os.makedirs(second_arg)
+first_arg = sys.argv[1]
+second_arg = sys.argv[2]
 
-  for item in os.listdir(first_arg):
-    if item.endswith(".jpg"):
-      img = Image.open(os.path.join(first_arg, item))
-      output_filename = os.path.splitext(item)[0] + ".png"
-      output_path = os.path.join(second_arg, output_filename)
-      img.save(output_path, "PNG")
+if not os.path.exists(second_arg):
+  os.makedirs(second_arg)
 
-else:
-  print("Please provide at least two arguments.")
+for item in os.listdir(first_arg):
+  if item.endswith(".jpg"):
+    img = Image.open(os.path.join(first_arg, item))
+    output_filename = os.path.splitext(item)[0] + ".png"
+    output_path = os.path.join(second_arg, output_filename)
+    img.save(output_path, "PNG")
+
