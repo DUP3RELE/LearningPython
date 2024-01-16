f24ddee0d3268f023b3b6da617f9cb8854275d96
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-01-16 19:14:58 +0000

    scraping hacker news.

--- tweetertwitty.py
+++ tweetertwitty.py
@@ -8,3 +8,17 @@ api = tweepy.API(auth)
 user = api.get_user(screen_name="Twitter")
 
 print(user.name)
+
+def limit_handler(cursor):
+  try:
+    while True:
+      yield cursor.next()
+  except tweepy.TweepError as e:
+    if 'Rate limit exceeded' in e.reason:
+      time.sleep(1000)
+    else:
+      raise
+# generous bot
+for follower in tweepy.Cursor(api.followers).items():
+
+
