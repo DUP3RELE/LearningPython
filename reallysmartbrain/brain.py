b41574796972bf55d3cc17a09f9407f537013d72
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2024-02-18 15:17:59 +0000

    add

--- reallysmartbrain/brain.py
+++ reallysmartbrain/brain.py
@@ -9,3 +9,15 @@ prediction.setModelPath(os.path.join(execution_path , "resnet50_weights_tf_dim_o
 
 gay = 1 + 2
 print(gay)
+
+from imageai.Predicition import ImagePrediction
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsSqueezeNet()
+prediction.setModelPath(os.path.join(execution_path , "resnet50_weights_tf_dim_ordering_" + "channels_last" + ".h5"))
+
+gay = 1 + 2
+print(gay)
