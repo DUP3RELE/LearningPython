7cd08aa07ca4aaabb1def4d863a3c6b418bd7b21
Krystian Żywczak (DUP3RELE) <97246430+DUP3RELE@users.noreply.github.com>
2024-02-25 22:06:26 +0000

    hello

--- reallysmartbrain/brain.py
+++ reallysmartbrain/brain.py
@@ -18,6 +18,3 @@ execution_path = os.getcwd()
 prediction = ImagePrediction()
 prediction.setModelTypeAsSqueezeNet()
 prediction.setModelPath(os.path.join(execution_path , "resnet50_weights_tf_dim_ordering_" + "channels_last" + ".h5"))
-
-gay = 1 + 2
-print('hello there')
